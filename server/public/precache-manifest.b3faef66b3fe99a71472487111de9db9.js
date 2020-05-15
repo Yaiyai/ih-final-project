49aff8f1307d66c543ac2eff2beef6dfdedc5656
Yai <myd.rio@hotmail.com>
2020-05-15 08:48:40 +0200

    trying deploy

---
+++ server/public/precache-manifest.b3faef66b3fe99a71472487111de9db9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a930b1902854461517091a2edc2a616d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce278a7895a2b9656127",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "69a74cf64544c513010c",
+    "url": "/static/css/main.67aa21c1.chunk.css"
+  },
+  {
+    "revision": "ce278a7895a2b9656127",
+    "url": "/static/js/2.a3d59cbc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a3d59cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a74cf64544c513010c",
+    "url": "/static/js/main.b5df9a2c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);

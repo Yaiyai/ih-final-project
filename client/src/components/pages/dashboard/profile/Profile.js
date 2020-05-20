7e02665079f09b914b10f99c724854d2f95e3234
Yai <myd.rio@hotmail.com>
2020-05-20 20:20:30 +0200

    profile styles

--- client/src/components/pages/dashboard/profile/Profile.js
+++ client/src/components/pages/dashboard/profile/Profile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 
 import UserServices from '../../../../service/user.service'
 
@@ -50,36 +49,30 @@ class Profile extends Component {
 	render() {
 		return (
 			<>
-				<section className='profilePage'>
+				<section className='profile-page'>
 					<h4>{this.props.loggedInDash.username}, aquí puedes editar tu perfil</h4>
-					<figure className='profileImg'>
-						{this.props.loggedInDash.avatar ? <img src={this.props.loggedInDash.avatar} alt='Cuentame más' /> : <img src='/imgs/ic/ic-signup.svg' alt='Cuentame más' />}
-					</figure>
 					<Form onSubmit={this.submitEdits}>
-						<Form.Group>
-							<Form.Label>Nombre de usuario</Form.Label>
-							<Form.Control name='username' type='text' onChange={this.handleChange} placeholder={this.props.loggedInDash.username} value={this.state.username} />
-						</Form.Group>
-						<input className='form-upload' name='newAvatar' type='file' onChange={this.handleUpload} />
-						<Form.Group>
-							<Form.Label>Email address</Form.Label>
-							<Form.Control name='email' type='email' onChange={this.handleChange} placeholder={this.props.loggedInDash.email} value={this.state.email} />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Nombre</Form.Label>
-							<Form.Control name='name' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.name} value={this.state.name} />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Apellidos</Form.Label>
-							<Form.Control name='lastName' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.lastName} value={this.state.lastName} />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label>Teléfono</Form.Label>
-							<Form.Control name='phone' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.phone} value={this.state.phone} />
-						</Form.Group>
-						<Button variant='primary' type='submit'>
+						<figure className='profile-img'>
+							{this.props.loggedInDash.avatar ? <img src={this.props.loggedInDash.avatar} alt='Cuentame más' /> : <img src='/imgs/ic/ic-signup.svg' alt='Cuentame más' />}
+						</figure>
+
+						<Form.Label className='form-label'>Editar avatar</Form.Label>
+						<Form.Control className='form-input' className='form-upload' name='newAvatar' type='file' onChange={this.handleUpload} />
+
+						<Form.Label className='form-label'>Nombre de usuario</Form.Label>
+						<Form.Control className='form-input' name='username' type='text' onChange={this.handleChange} placeholder={this.props.loggedInDash.username} value={this.state.username} />
+						<Form.Label>Email address</Form.Label>
+						<Form.Control className='form-input' name='email' type='email' onChange={this.handleChange} placeholder={this.props.loggedInDash.email} value={this.state.email} />
+						<Form.Label>Nombre</Form.Label>
+						<Form.Control className='form-input' name='name' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.name} value={this.state.name} />
+						<Form.Label>Apellidos</Form.Label>
+						<Form.Control className='form-input' name='lastName' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.lastName} value={this.state.lastName} />
+						<Form.Label>Teléfono</Form.Label>
+						<Form.Control className='form-input' name='phone' onChange={this.handleChange} type='text' placeholder={this.props.loggedInDash.phone} value={this.state.phone} />
+
+						<button className='my-button mini' type='submit'>
 							Guardar cambios
-						</Button>
+						</button>
 					</Form>
 				</section>
 			</>
